985396dbca384235e59027f3e848fbdcf88b50d2
kushal <kushaldeveloper@gmail.com>
2017-04-10 17:42:17 -0400

    build automatically

--- dist/main.bundle.js
+++ dist/main.bundle.js
@@ -560,7 +560,7 @@ var AppModuleNgFactory = new __WEBPACK_IMPORTED_MODULE_0__angular_core__["y" /* 
 /* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "a", function() { return AppComponent; });
 var AppComponent = (function () {
     function AppComponent() {
-        this.title = 'This app works!';
+        this.title = 'This app does not work!';
     }
     return AppComponent;
 }());
