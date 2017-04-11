50d2898861f123409c5b7b47e61a1473b621b081
kushal <kushaldeveloper@gmail.com>
2017-04-10 20:52:07 -0400

    build automatically

--- dist/main.bundle.js
+++ dist/main.bundle.js
@@ -576,8 +576,8 @@ var AppModuleNgFactory = new __WEBPACK_IMPORTED_MODULE_0__angular_core__["y" /* 
 /* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "a", function() { return AppComponent; });
 var AppComponent = (function () {
     function AppComponent() {
-        this.title = "Do you know where your children are?";
-        this.body = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed congue tellus at ex tempor sodales. Nulla nec eros ante. Fusce bibendum fermentum mauris, vitae congue erat rutrum in. Pellentesque consectetur, erat ac pellentesque facilisis, sem est rutrum purus, vel efficitur felis leo at libero. Nulla vulputate enim quis ultricies vulputate. Sed consequat nulla lorem. Maecenas tincidunt malesuada enim, a rutrum elit ornare sit amet. Etiam odio orci, pharetra quis facilisis eu, pretium et mauris. Maecenas rutrum, dolor mollis ullamcorper varius, nisi tortor luctus tortor, ornare molestie tellus ante eu metus. Proin consequat nisi at lorem varius, sit amet congue arcu gravida. Donec vulputate sagittis venenatis.";
+        this.title = "A better web for the children";
+        this.body = "Let us talk about what we can do to make the web better... together! ";
         this.quote = "cd /path/to/project && git pull origin master && /usr/bin/ng build --aot && git add . && git commit -m \"build automatically\" && git push origin master &&  rsync -av /path/to/project/ /path/to/html/repo/ && cd /path/to/html/repo/ && git add . && git commit -m \"add everything\" && git push -u origin --all";
     }
     return AppComponent;
