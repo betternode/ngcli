516e3a2c501221c03f841aefc3c14d9c96c3f4b0
kushal <kushaldeveloper@gmail.com>
2017-04-10 23:06:59 -0400

    add copyright

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,4 +9,5 @@ export class AppComponent {
   title = "A better web for the children";
   body = "Let us talk about what we can do to make the web better... together! ";
   quote = "cd /path/to/project && git pull origin master && /usr/bin/ng build --aot && git add . && git commit -m \"build automatically\" && git push origin master &&  rsync -av /path/to/project/ /path/to/html/repo/ && cd /path/to/html/repo/ && git add . && git commit -m \"add everything\" && git push -u origin --all";
+  copyright = "&copy; 2017";
 }
