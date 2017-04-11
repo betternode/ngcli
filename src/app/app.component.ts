1b247c8a8343f4cef2ca10e243047b38ea8b3950
kushal <kushaldeveloper@gmail.com>
2017-04-10 21:09:20 -0400

    add a body

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,5 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Do you know where your children are?';
+  body = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed congue tellus at ex tempor sodales. Nulla nec eros ante. Fusce bibendum fermentum mauris, vitae congue erat rutrum in. Pellentesque consectetur, erat ac pellentesque facilisis, sem est rutrum purus, vel efficitur felis leo at libero. Nulla vulputate enim quis ultricies vulputate. Sed consequat nulla lorem. Maecenas tincidunt malesuada enim, a rutrum elit ornare sit amet. Etiam odio orci, pharetra quis facilisis eu, pretium et mauris. Maecenas rutrum, dolor mollis ullamcorper varius, nisi tortor luctus tortor, ornare molestie tellus ante eu metus. Proin consequat nisi at lorem varius, sit amet congue arcu gravida. Donec vulputate sagittis venenatis.';
 }
